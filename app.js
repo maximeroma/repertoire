3342aa08cb16bf630cd65cdcabc968e090412f6e
romamaxime <roma.maxime1994@gmail.com>
2017-03-31 15:32:39 +0200

    add plugin jquery tablesorter

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
+	
+	var jqObj;
+	var annuary = [];
+
+	$("#myTable").delegate('thead', 'click', function(){
+		$("tbody").children().remove();
+		for( var i = 0; i < annuary.length; i++ ){
+			$("#myTable").append("<tr></tr>");			
+			jqObj = $("tr").last().append("<td>" + i + "</td>")
+									.append("<td>" + annuary[i]["prenom"] + "</td>")
+									.append("<td>" + annuary[i]["nom"] + "</td>")
+									.append("<td>" + annuary[i]["age"] + "</td>")
+									.append("<button class='btn btn-secondary deleteButton'>X</button>");
+			jqObj.data('ID', i);
+			console.log(jqObj.data('ID', i));
+		};
+		$("#myTable").tablesorter({theme : 'bootstrap_3'}).trigger("update");
+    }); 
+	
+	
 
 	
-	var annuary = [];
-	var count = 0;
 
 	var functionSubmit = function(){
 		$("#buttonSubmit").click(function(){
-			var contact = { "nom" : $("#idNom").val(),
-							"prenom" : $("#idPrenom").val(),
-							"age" : $("#idAge").val()
+			var contact = { "nom" : 	$("#idNom").val(),
+							"prenom" : 	$("#idPrenom").val(),
+							"age" : 	$("#idAge").val()
 			};
 
 			$("#idNom").val('');
@@ -27,11 +45,14 @@ $(document).ready(function(){
 			
 			$("tbody").children().remove();
 			for( var i = 0; i < annuary.length; i++ ){
-
-
 				$("#myTable").append("<tr></tr>");
-				$("tr").last().append("<th scope='row'>" + (i) + "</th>").append("<td>" + annuary[i]["prenom"] + "</td>").append("<td>" + annuary[i]["nom"] + "</td>").append("<td>" + annuary[i]["age"] + "</td>").append("<button class='btn btn-secondary deleteButton'>X</button>");
-				
+				var jqObj = $("tr").last().append("<th scope='row'>" + i + "</th>")
+											.append("<td>" + annuary[i]["prenom"] + "</td>")
+											.append("<td>" + annuary[i]["nom"] + "</td>")
+											.append("<td>" + annuary[i]["age"] + "</td>")
+											.append("<button class='btn btn-secondary deleteButton'>X</button>");
+				jqObj.data('ID', i);
+				//console.log(jqObj.data())
 			};
 		});
 	}; 
@@ -40,7 +61,22 @@ $(document).ready(function(){
 	
 	var deleteTableRow = function(){
 		$("#myTable").delegate(".deleteButton", 'click', function(){
+			//console.log($(this).parent().data('ID'));
+			annuary.splice('ID',1);
+			//console.log(annuary);
 			$(this).parent().remove();
+			$("tbody").children().remove();
+			for( var i = 0; i < annuary.length; i++ ){
+				$("#myTable").append("<tr></tr>");
+				var jqObj = $("tr").last().append("<th scope='row'>" + i + "</th>")
+											.append("<td>" + annuary[i]["prenom"] + "</td>")
+											.append("<td>" + annuary[i]["nom"] + "</td>")
+											.append("<td>" + annuary[i]["age"] + "</td>")
+											.append("<button class='btn btn-secondary deleteButton'>X</button>");
+				jqObj.data('ID', i);
+				//console.log(jqObj.data())
+			};
+			
 		});
 	}; 
 
