dfc2555f7c006607a9f084489a1ba8a57b777a45
romamaxime <roma.maxime1994@gmail.com>
2017-03-31 10:57:16 +0200

    add function deleteRow

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
 $(document).ready(function(){
 
 	
-	var functionContact = function(){		
-		
-		$("#idContact").on('change', function(){
-			var saveContact = $(this).val();	
-			console.log(saveContact);	
-		});
-	};
+	var annuary = [];
+	var count = 0;
 
-	functionContact();
+	var functionSubmit = function(){
+		$("#buttonSubmit").click(function(){
+			var contact = { "nom" : $("#idNom").val(),
+							"prenom" : $("#idPrenom").val(),
+							"age" : $("#idAge").val()
+			};
+
+			$("#idNom").val('');
+			$("#idPrenom").val('');
+			$("#idAge").val('');
+
+			annuary.push(contact);
+			//console.log(annuary);
+			
+			contact = { "nom" : "",
+						"prenom" : "",
+						"age" : ""
+			};
+			
+			//console.log(contact);
+			
+			$("tbody").children().remove();
+			for( var i = 0; i < annuary.length; i++ ){
+
+
+				$("#myTable").append("<tr></tr>");
+				$("tr").last().append("<th scope='row'>" + (i) + "</th>").append("<td>" + annuary[i]["prenom"] + "</td>").append("<td>" + annuary[i]["nom"] + "</td>").append("<td>" + annuary[i]["age"] + "</td>").append("<button class='btn btn-secondary deleteButton'>X</button>");
+				
+			};
+		});
+	}; 
 	
-
-	var functionPrenom = function(){
-
-		$("#idPrenom").on('change', function(){
-			var savePrenom = $(this).val();	
-			console.log(savePrenom);	
+	functionSubmit();
+	
+	var deleteTableRow = function(){
+		$("#myTable").delegate(".deleteButton", 'click', function(){
+			$(this).parent().remove();
 		});
-	};
+	}; 
 
-	functionPrenom();
-
-	var functionNom = function(){
-
-		$("#idNom").on('change', function(){
-			var saveNom = $(this).val();	
-			console.log(saveNom);	
-		});
-	};
-
-	functionNom();
-
-	var functionAge = function(){
-
-		$("#idAge").on('change', function(){
-			var saveAge = $(this).val();	
-			console.log(saveAge);	
-		});
-	};
-	functionAge();
+	deleteTableRow();
 
 });
